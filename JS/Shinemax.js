bb913c767db192a8365ee3cb78c26aa26f0fe519
@jhonathaGC <gabrielljjhonatha@gmail.com>
2023-05-12 15:32:30 -0300

    Arrumando Detalhes @TheoMesquita

--- JS/Shinemax.js
+++ JS/Shinemax.js
@@ -28,6 +28,30 @@ function mostrar(el) {
 
 }
 
+/* Botão Conteudo mover "Esquerda" e "Direita" */
+
+const scroll = document.getElementById("scroll")
+function moverEsquerda() {
+  scroll.scrollLeft-=100
+}
+function moverDireita() {
+  scroll.scrollLeft+=100
+}
+const scroll2 = document.getElementById("scroll2")
+function moverEsquerda2() {
+  scroll2.scrollLeft-=100
+}
+function moverDireita2() {
+  scroll2.scrollLeft+=100
+}
+const scroll3 = document.getElementById("scroll3")
+function moverEsquerda3() {
+  scroll3.scrollLeft-=100
+}
+function moverDireita3() {
+  scroll3.scrollLeft+=100
+}
+
 /* Gráfico */
 
 var data = {
@@ -57,3 +81,7 @@ var data = {
     }
 };
   const myChart = new Chart(document.getElementById("avaliacoes"), config);
+
+  function like() {
+    data.data.length[1]
+  }
