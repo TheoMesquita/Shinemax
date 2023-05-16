0c4828760f9a73146d46d549161c1857227a1116
Aluno <theofilo.mesquita@sptech.school>
2023-05-16 15:16:53 -0300

    @theo

---
+++ JS/Pesquisar.js
@@ -0,0 +1,13 @@
+/* Botão de Pesquisa */
+
+function mostrar(el) {
+    var display = document.getElementById(el).style.display;
+    if(display == "none"){
+
+        document.getElementById(el).style.display = 'flex'
+        document.getElementById(el).className = 'pesquisa'
+    } else {
+        document.getElementById(el).style.display = 'none';
+    }
+
+}
--- JS/Shinemax.js
+++ JS/Shinemax.js
@@ -14,20 +14,6 @@ function Carrossel() {
 }
 setInterval(Carrossel, 2000);
 
-/* Botão de Pesquisa */
-
-function mostrar(el) {
-    var display = document.getElementById(el).style.display;
-    if(display == "none"){
-
-        document.getElementById(el).style.display = 'flex'
-        document.getElementById(el).className = 'pesquisa'
-    } else {
-        document.getElementById(el).style.display = 'none';
-    }
-
-}
-
 /* Botão Conteudo mover "Esquerda" e "Direita" */
 
 const scroll = document.getElementById("scroll")
