2925d839595c93cb7de4f8337b28768d26d4bbd7
Sarah <sarah.silva@sptech.school>
2023-06-02 17:06:19 -0300

    Pequena alteração na API @TheoMesquita

--- Site/Src/Models/graficoModel.js
+++ Site/Src/Models/graficoModel.js
@@ -25,7 +25,7 @@ function cadastrarAvaliacao(avaliacao) {
     // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
     //  e na ordem de inserção dos dados.
     var instrucao = `
-          INSERT INTO voto (nota, momento, fkUsuarioVoto) VALUES ('${avaliacao}', now(), 1);
+          INSERT INTO voto (nota) VALUES ('${avaliacao}');
           `;
     console.log("Executando a instrução SQL: \n" + instrucao);
     return database.executar(instrucao);
@@ -36,10 +36,10 @@ function buscarValoresTempoReal(idVoto) {
     instrucaoSql = ''
 
     if (process.env.AMBIENTE_PROCESSO == "producao") {
-        instrucaoSql = `select nota, count(fkUsuarioVoto) from voto group by nota order by nota;`;
+        instrucaoSql = `select count(nota) from voto group by nota order by nota;`;
 
     } else if (process.env.AMBIENTE_PROCESSO == "desenvolvimento") {
-        instrucaoSql = `select nota, count(fkUsuarioVoto) from voto group by nota order by nota;`;
+        instrucaoSql = `select count(nota) from voto group by nota order by nota;`;
     } else {
         console.log("\nO AMBIENTE (produção OU desenvolvimento) NÃO FOI DEFINIDO EM app.js\n");
         return
